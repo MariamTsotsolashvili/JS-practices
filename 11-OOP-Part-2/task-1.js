0fb82265c5265e0277ed1b3b4a6a95003997ebb7
MariamTsotsolashvili <mariami.tsotsolashvili.1@btu.edu.ge>
2019-06-07 20:21:22 +0400

    feat(11-oop-part-2): Finish tasks

---
+++ 11-OOP-Part-2/task-1.js
@@ -0,0 +1,21 @@
+
+Object.prototype.delay = function(ms) {
+
+    return  (...args) => {
+      setTimeout(() => {
+        this(...args)
+      }, ms);
+    }
+}
+
+
+function f() {
+    console.log('hello');
+  }
+  f.delay(1000)(); // print "hello" after 1 second
+
+  // or with parameters:
+  function f2(a, b) {
+    console.log(a + b);
+  }
+  f2.delay(1000)(1, 2); // print 3 after 1 second
